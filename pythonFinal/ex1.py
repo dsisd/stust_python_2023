9943409d88fd118f66e966f7e0d45dd9dddd04d7
dsisd <112936095+dsisd@users.noreply.github.com>
2024-01-04 11:07:44 +0800

    feat

--- pythonFinal/ex1.py
+++ pythonFinal/ex1.py
@@ -3,6 +3,7 @@ import os
 
 class FryChicken:
     def __init__(self, fryChicken, Fries, Soda, Salad, Flavor):
+        # 初始化餐點設定
         self.setup = {}
         self._soda = Soda
         self._fries = Fries
@@ -11,43 +12,51 @@ class FryChicken:
         self._flavor = Flavor
 
     def add_combo(self, easyCombo):
+        # 添加套餐到設定中
         for combo in easyCombo:
-            self.setup.setdefault(combo[2], []).append({"main meal": combo[0], "Comes with meal": combo[1]})
+            self.setup.setdefault(combo[2], []).append({"主餐": combo[0], "附餐": combo[1]})
 
     def delete_combo(self, setup_fryChicken, easyCombo):
+        # 刪除特定套餐中的主餐
         setup_easyCombo = self.setup.get(easyCombo, [])
-        self.setup[easyCombo] = [_setup for _setup in setup_easyCombo if _setup["main meal"] != setup_fryChicken]
-        print(f"Setup {setup_fryChicken} deleted for {self._fryChicken} in setup {easyCombo}.")
+        self.setup[easyCombo] = [_setup for _setup in setup_easyCombo if _setup["主餐"] != setup_fryChicken]
+        print(f"在 {easyCombo} 中，刪除 {self._fryChicken} 的設定 {setup_fryChicken}.")
 
     def get_setup(self, easyCombo):
+        # 獲取特定套餐的設定
         return self.setup.get(easyCombo, [])
 
     def save_to_file(self, filename):
+        # 將餐點設定儲存至檔案
         with open(filename, 'w') as file:
-            json.dump({"Main Meal": self._fryChicken, "Comes with meal": self._fries, "Soda": self._soda}, file)
+            json.dump({"主餐": self._fryChicken, "附餐": self._fries, "汽水": self._soda}, file)
 
     def load_from_file(self, filename):
+        # 從檔案載入餐點設定
         if os.path.exists(filename):
             with open(filename, 'r') as file:
                 data = json.load(file)
-                self._fryChicken, self._fries, self._soda = data.get("Main Meal"), data.get("Comes with meal"), data.get("Soda", {})
+                self._fryChicken, self._fries, self._soda = data.get("主餐"), data.get("附餐"), data.get("汽水", {})
 
-def create_or_load_student_profile(fryChicken, Fries, Cola, Salad, Flavor):
+def create_or_load_meal(fryChicken, Fries, Cola, Salad, Flavor):
+    # 創建或載入餐點
     setup_filename = f"{fryChicken}_profile.json"
     setup = FryChicken(fryChicken, Fries, Cola, Salad, Flavor)
     setup.load_from_file(setup_filename)
     return setup, setup_filename
 
 def add_combo(setup, *easycombo):
+    # 添加自由搭配餐點
     setup.add_combo(easycombo)
 
-#顯示套餐
+# 顯示套餐
 def display_setup(setup_fryChicken, setup_fries, setup_soda, easyCombo, setup_taken):
-    print(f"Setup taken by {setup_fryChicken}, {setup_fries} , {setup_soda} in {easyCombo}:")
+    print(f"{easyCombo} 中的設定，由 {setup_fryChicken}、{setup_fries} 和 {setup_soda} 取得的套餐:")
     for setup in setup_taken:
-        print(f"{setup['main meal']}: {setup['Comes with meal']}")
+        print(f"{setup['主餐']}: {setup['附餐']}")
 
 def delete_and_save_course(setup, fryChicken, easyCombo):
+    # 刪除餐點並儲存至檔案
     setup.delete_combo(fryChicken, easyCombo)
     setup.save_to_file(setup_filename)
 
@@ -58,21 +67,21 @@ Soda = "cola"
 Salad = "Salad"
 Flavor = "hot"
 
-# 建立餐點
-setup, setup_filename = create_or_load_student_profile(fryChicken, Fries, Soda, Salad, Flavor)
+# 建立或載入餐點
+setup, setup_filename = create_or_load_meal(fryChicken, Fries, Soda, Salad, Flavor)
 
 # 添加餐點
-setup_to_add = [("Comes with meal", "fries", "easycombo")]
+setup_to_add = [("附餐", "fries", "自由搭配")]
 add_combo(setup, *setup_to_add)
 
 # 儲存至檔案
 setup.save_to_file(setup_filename)
 
-# 顯示特定 自由搭配 的餐點
-setup_to_search = "easycombo"
+# 顯示特定自由搭配的餐點
+setup_to_search = "自由搭配"
 courses_taken = setup.get_setup(setup_to_search)
 display_setup(fryChicken, Fries, Soda, setup_to_search, courses_taken)
 
 # 刪除餐點
-course_to_delete = "main meal"
+course_to_delete = "主餐"
 delete_and_save_course(setup, course_to_delete, setup_to_search)
